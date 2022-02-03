7551b46f0867c13d5d00c10621123eaf73e41285
Zeykroz <64717198+Zeykroz@users.noreply.github.com>
2022-02-02 23:14:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const btn_play = document.getElementById('btn_play');
 const btn_step = document.getElementById('btn_step');
 const timer = document.getElementById('timer');
 
-
 btn_play.addEventListener('click', () => {
     if (video.paused) {
         video.play();
@@ -14,14 +13,14 @@ btn_play.addEventListener('click', () => {
         btn_play.innerHTML = '<i class="fas fa-play"></i>';
     }
 });
-
+//Funcion para establecer el tiempo de reproduccion
 function setTimer(currentTime) {
     let minutes = Math.floor(currentTime / 60);
     let seconds = Math.floor(currentTime - minutes * 60);
     let time = `${minutes<10 ? '0'+minutes:minutes}:${seconds<10 ? '0'+seconds:seconds}`;
     return timer.innerHTML = time;
 }
-
+//Evento de reproducción del video
 video.addEventListener('timeupdate', () => {
     if (video.currentTime >= video.duration) {
         video.pause();
@@ -29,20 +28,29 @@ video.addEventListener('timeupdate', () => {
     }
     ctrlTimeVideo.value = video.currentTime / video.duration * 1;
     timer.innerHTML = setTimer(video.currentTime);
-    
 });
-
+//Evento de cambio de tiempo mientras se manipula el slider
+ctrlTimeVideo.addEventListener('input', () => {
+    if (!video.paused) {
+        video.pause();
+        video.attributes.preselected = true;
+        btn_play.innerHTML = '<i class="fas fa-play"></i>';
+    }
+    video.currentTime = ctrlTimeVideo.value * video.duration;
+});
+//Evento para confirmar el cambio de tiempo
 ctrlTimeVideo.addEventListener('change', function (e) {
-    video.currentTime = e.currentTarget.value * video.duration;
-    timer.innerHTML = setTimer(video.currentTime);
+    if(video.attributes.preselected){
+        video.play()
+        btn_play.innerHTML = '<i class="fas fa-pause"></i>'
+    }
 });
-
+//Evento para pasar al siguiente frame
 btn_step.addEventListener('click', () => {
     video.currentTime += video.duration*0.10;
     timer.innerHTML = setTimer(video.currentTime);
 });
-
-//DETECTAR TECLA FLECHA IZQUIERDA Y DERECHA PARA AVANZAR un 10 porciento
+//Evento para las flechas del teclado
 document.addEventListener('keydown', (e) => {
     if (e.key === 'Space') {
         if (video.paused) {
@@ -60,5 +68,4 @@ document.addEventListener('keydown', (e) => {
         video.currentTime += video.duration*0.10;
         timer.innerHTML = setTimer(video.currentTime);
     }
-    
 });
